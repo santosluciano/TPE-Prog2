192c7158572e218f17b00259d354b452da48cb6c
Luciano Santos <luchosan74@gmail.com>
2017-09-21 17:25:49 -0300

    Agregadas clases Carta4-5-6

---
+++ JuegoCartas/src/Carta4.java
@@ -0,0 +1,10 @@
+
+public class Carta4 extends Carta{
+	public Carta4(String n,Atributo a,Atributo b,Atributo c,Atributo d) {
+		super(n);
+		super.add(a);
+		super.add(b);
+		super.add(c);
+		super.add(d);
+	}
+}
---
+++ JuegoCartas/src/Carta6.java
@@ -0,0 +1,12 @@
+
+public class Carta6 extends Carta{
+	public Carta6(String n,Atributo a,Atributo b,Atributo c,Atributo d,Atributo e,Atributo f) {
+		super(n);
+		super.add(a);
+		super.add(b);
+		super.add(c);
+		super.add(d);
+		super.add(e);
+		super.add(f);
+	}
+}
