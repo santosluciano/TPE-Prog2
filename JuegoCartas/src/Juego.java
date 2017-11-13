b5b65bc67cc6deb20a387b00f70a798f1bfae644
Meliendrez Agustin <ameliendrez@gmail.com>
2017-11-13 11:34:49 -0300

    Menu implementado... sin fallas??

--- JuegoCartas/src/Juego.java
+++ JuegoCartas/src/Juego.java
@@ -14,81 +14,94 @@ public class Juego {
 		int mazoSeleccionado;
 		String nombreJ1;
 		String nombreJ2;
+		String volverMenu = "";
 		Mazo mazo;
      	JuegoCartas partida;
+     	
+     	do {
 
 
-
-
-
-
-
-		do {
-			System.out.println ("1.Partida normal");
-			System.out.println ("2.Partida por turnos");
-			System.out.println ("3.Cargar mazo");
-			System.out.println ("4.Salir del juego");
-
-			opcion = obtnerValor();
-		}while(opcion<=0||opcion>=5); 
-
-
-		switch (opcion) {
-		case 1: 
-			 
-			System.out.println ("¿¿Con que mazo quieres jugar??");
-			System.out.println ("ingrese Valor numerico");
-
-
-			for (int i = 0; i < mazos.size(); i++) {
-				System.out.println("opcion "+ (i+1) + ": " + mazos.get(i).getNombreMazo());
-			}
-			mazoSeleccionado = obtnerValor();
+			do {
+				System.out.println ("1.Partida normal");
+				System.out.println ("2.Partida por turnos");
+				System.out.println ("3.Cargar mazo");
+				System.out.println ("4.Salir del juego");
+	
+				opcion = obtnerValor();
+			}while(opcion<1||opcion>4); 
+	
+	
+			switch (opcion) {
+			case 1: 
+				 
+				System.out.println ("¿¿Con que mazo quieres jugar??");
+				System.out.println ("ingrese Valor numerico");
+	
+				getNombreMazo(mazos);
+				
+				do {
+					mazoSeleccionado = obtnerValor();
+				} while (mazoSeleccionado <=0 && mazoSeleccionado >mazos.size());
+				
+				mazo = mazos.get(mazoSeleccionado-1);
+				
+				System.out.println ("ingrese Nombre de los jugadores");
+				System.out.print ("Nombre de Jugador 1: ");
+				nombreJ1 = obtenerNombre();
+				
+				System.out.print ("Nombre de Jugador 2: ");
+				nombreJ2 = obtenerNombre();
+	
+				
+				partida = new JuegoCartas(nombreJ1,nombreJ2,mazo);
+	     		partida.jugar();
+				break;
+			case 2:
+				System.out.println ("¿¿Con que mazo quieres jugar??");
+				System.out.println ("ingrese Valor numerico");
+	
+				getNombreMazo(mazos);
+				
+				do {
+					mazoSeleccionado = obtnerValor();
+				} while (mazoSeleccionado <=0 && mazoSeleccionado >mazos.size());
+				
+				mazo = mazos.get(mazoSeleccionado-1);
+				
+				System.out.println ("ingrese Nombre de los jugadores");
+				System.out.print ("Nombre de Jugador 1: ");
+				nombreJ1 = obtenerNombre();
+				
+				System.out.print ("Nombre de Jugador 2: ");
+				nombreJ2 = obtenerNombre();
+				
+	        	System.out.println ("Cuantos turnos va a durar la partida?");
+	     		opcion = obtnerValor();
+	     		partida = new JuegoTurnos(nombreJ1,nombreJ2,mazo,opcion);
+	     		partida.jugar();;
+	     		break;
+			case 3:
+				mazos.addAll(cargarMazo());	
+				
+				for (Mazo mazoIterado : mazos) {
+					if(mazoIterado.vacio()) {
+						mazos.remove(mazoIterado);
+					}
+				}
+				break;
+			case 4:
+				System.out.println ("Gracias por jugar");
+				break;
+			};
 			
-			mazo = mazos.get(mazoSeleccionado);
+				volverMenu = volverMenu();
 			
-			System.out.println ("ingrese Nombre de los jugadores");
-			System.out.print ("Nombre de Jugador 1: ");
-			nombreJ1 = obtenerNombre();
-			
-			System.out.print ("Nombre de Jugador 2: ");
-			nombreJ2 = obtenerNombre();
+     	} while (volverMenu.equals("y"));
+     	
+     	
+		System.out.println("Gracias por jugar");
 
-			
-			partida = new JuegoCartas(nombreJ1,nombreJ2,mazo);
-     		partida.jugar();
-			break;
-		case 2:
-			System.out.println ("¿¿Con que mazo quieres jugar??");
-			System.out.println ("ingrese Valor numerico");
-
-
-			for (int i = 0; i < mazos.size(); i++) {
-				System.out.println("opcion "+ (i+1) + ": " + mazos.get(i).getNombreMazo());
-			}
-			mazoSeleccionado = obtnerValor();
-			
-			mazo = mazos.get(mazoSeleccionado);
-			
-			System.out.println ("ingrese Nombre de los jugadores");
-			System.out.print ("Nombre de Jugador 1: ");
-			nombreJ1 = obtenerNombre();
-			
-			System.out.print ("Nombre de Jugador 2: ");
-			nombreJ2 = obtenerNombre();
-			
-        	System.out.println ("Cuantos turnos va a durar la partida?");
-     		opcion = obtnerValor();
-     		partida = new JuegoTurnos(nombreJ1,nombreJ2,mazo,opcion);
-     		partida.jugar();;
-		case 3:
-			mazos.addAll(cargarMazo());				
-			break;
-		case 4:
-			System.out.println ("Gracias por jugar");
-			break;
-		};
-
+     	
 //		for(Mazo seleccionMazo:mazos) {
 //			System.out.println(seleccionMazo.toString());
 //			System.out.println("!!!!!!!!!!!!!!!!!!");				
@@ -105,18 +118,60 @@ public class Juego {
 		//			Filtro compuesto = new OR(mayor,menor);
 		//			Mazo mazoFiltrado = nuevoMazo.getCartas(compuesto);
 		//			System.out.println(mazoFiltrado.toString());
+		
+	}
+	
+	public static String volverMenu(){
+		String opcion = "";
+		
+		do  {
+			System.out.println("¿Quieres volver al menu? ingrese y/n");
+			opcion = obtenerNombre();
+		}while ((!opcion.equals("n")) && (!opcion.equals("y")));
+		
+		return opcion;
+	}
+	
+	public static void getNombreMazo(List <Mazo> mazos){
+		
+			for (int i = 0; i < mazos.size(); i++) {
+				System.out.println("opcion "+ (i+1) + ": " + mazos.get(i).getNombreMazo());
+			}
 	}
 
 	public static List<Mazo> cargarMazo() {
 		List <Mazo> mazos = new ArrayList<Mazo>();
-		String carpeta = "/home/lucho/Documentos/TPE-Prog2/JuegoCartas/cartas";
-		System.out.print("Ingrese el nombre del archivo con los datos del mazo: ");
-		String nombreMazo = obtenerNombre();//"minions.txt";
+		//String carpeta = "/home/lucho/Documentos/TPE-Prog2/JuegoCartas/cartas"; //computadora lucho
+		String carpeta = "C://Cartas/"; // coputadora agustin
+		String nombreMazo="";
+		boolean valido=true;;
+		
+		do {
+			System.out.println("Ingrese el nombre del archivo con los datos del mazo: ");
+
+			nombreMazo = obtenerNombre();//"minions.txt";
+			
+			if (!nombreMazo.equals("minions") && !nombreMazo.equals("minions") && !nombreMazo.equals("Mix2") && !nombreMazo.equals("Mix1")) {
+				System.out.println("Mazo no encontrado: ");
+				valido = false;
+			}
+			else if (nombreMazo.equals("minions") || nombreMazo.equals("minions") || nombreMazo.equals("Mix2") || nombreMazo.equals("Mix1")){
+				valido = true;
+			}
+
+		} while (!valido);
+		
+		
 		String mazoC1 = nombreMazo+".txt";
 		LectorCartas lector = new LectorCartas();
 		Vector<Hashtable> mazo1 = lector.leerMazo(carpeta, mazoC1);
+		
+		
+		
 		//String[] nombreMazo = mazoC1.split(".");
-		// System.out.println(mazoC1);	
+		// System.out.println(mazoC1);
+		
+		
 		Mazo nuevoMazo = new Mazo();
 		nuevoMazo.setNombreMazo(nombreMazo);
 		mazos.add(nuevoMazo);		
@@ -195,80 +250,6 @@ public class Juego {
 		return valor;
 	}
 	
-
-
-
-	
-	
-		//int cantidadCartas = 0;
-	 //		int primero = 0;
-	 //		int segundo = 0;
-	 //		int tercero = 0;
-	 //		int opcion = 0;
-	 //		String nombreMazo = "";
-	 //		String nombreCarta = "";
-	 //		String nombreJ1 = "";
-	 //		String nombreJ2 = "";
-	 //		
-	 //		System.out.println("¿Cuantas cartas tiene el mazo? ");
-	 //		cantidadCartas = obtnerValor();
-	 //		
-	 //		Mazo maso = new Mazo();
-	 //		for (int i=0;i<cantidadCartas;i) {
-	 //
-	 //			System.out.println("Nombre de la Carta: ");
-	 //			nombreCarta = obtenerNombreCarta();
-	 //				
-	 //			System.out.println("Fuerza: ");
-	 //			primero = obtnerValor();
-	 //			String a = "Fuerza";
-	 //
-	 //			System.out.println("Velocidad: ");
-	 //			segundo = obtnerValor();
-	 //			String b = "Velocidad";
-	 //
-	 //			System.out.println("Mental: ");
-	 //			tercero = obtnerValor();
-	 //			String c = "Mental";
-	 //			
-	 //			Carta nuevaCarta = new Carta ();
-	 //			nuevaCarta.addAtributo(a,primero);
-	 //			nuevaCarta.addAtributo(b,segundo);
-	 //			nuevaCarta.addAtributo(c,tercero);
-	 //			nuevaCarta.setNombre(nombreCarta);
-	 //		
-	 //			
-	 //			maso.addCarta(nuevaCarta);
-	 //		}
-	 //		
-	 //		System.out.println("¿Cual es el nombre del mazo creado? ");
-	 //		nombreMazo = obtenerNombreCarta();
-	 //		maso.setNombreMazo(nombreMazo);
-	 //		
-	 //		System.out.println("Nombre Jugador 1:");
-	 //		nombreJ1 = obtenerNombreCarta();
-	 //		System.out.println("Nombre Jugador 2:");
-	 //		nombreJ2 = obtenerNombreCarta();
-	 //    	JuegoCartas partida;
-	 //    	do {
-	 //    			System.out.println ("Elija el tipo de partida:");
-	 //    			System.out.println ("1.Partida normal");
-	 //    			System.out.println ("2.Partida por turnos");
-	 //    			opcion = obtnerValor();
-	 //    	}while(opcion!=1&&opcion!=2); 
-	 //        switch (opcion) {
-	 //        	case 1: 
-	 //        		partida = new JuegoCartas(nombreJ1,nombreJ2,maso);
-	 //        		partida.jugar();
-	 //        		break;
-	 //        	case 2:
-	 //        		System.out.println ("Cuantos turnos va a durar la partida?");
-	 //        		opcion = obtnerValor();
-	 //        		partida = new JuegoTurnos(nombreJ1,nombreJ2,maso,opcion);
-	 //        		partida.jugar();
-	 //        		break;
-	 //        };
-	 //	}
 
 }
 		
